432db451637eefa8dc5f67b17118f7487ef39ad4
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-18 08:34:35 -0500

    Add el modelo equipo

---
+++ TallerClase/Models/Equipo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerClase.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]       
+        public string Nombre { get; set; }
+
+        [MaxLength(100)]
+        public string Ciudad { get; set; }
+
+        public int Titulos { get; set; }
+
+        public bool AceptaExtranjeros { get; set; } 
+    }
+}
