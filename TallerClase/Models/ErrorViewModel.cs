51e5433cfb627efd277597fe844f6179f68e94e1
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-18 08:26:08 -0500

    Agregar archivos de proyecto.

---
+++ TallerClase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TallerClase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
