72d9f2e806fca3e25e263659a22bac561b6be991
JoaoConde12 <132232545+JoaoConde12@users.noreply.github.com>
2024-10-18 08:41:35 -0500

    Add los modelos de Jugador y Estadio

---
+++ TallerClase/Models/Estadio.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TallerClase.Models
+{
+    public class Estadio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(150)]
+        public string Direccion { get; set; }
+
+        [MaxLength(50)]
+        public string Ciudad { get; set; }
+
+        public float Capacidad { get; set; }
+    }
+}
---
+++ TallerClase/Models/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TallerClase.Models
+{
+    public class Jugador
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+
+        [MaxLength(50)]
+        public string Posicion { get; set; }
+        public int Edad { get; set; }
+
+        [Required]
+        public Equipo? Equipo { get; set; }
+
+        [ForeignKey(nameof(Equipo))]
+        public int IdEquipo { get; set; }
+    }
+}
